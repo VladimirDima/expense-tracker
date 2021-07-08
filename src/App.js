044eaed05fa53d45f94536db0052f039ecec179d
Vladimir Dima <dimavladimir14@gmail.com>
2021-07-08 18:53:53 +0200

    added form and gitignore

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
